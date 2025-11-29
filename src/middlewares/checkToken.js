9136350a4e827f55b69381dad0fc66fe2b375e19
Felipe <fe.moralesc@duocuc.cl>
2025-11-29 18:47:49 -0300

    cambio check token

--- src/middlewares/checkToken.js
+++ src/middlewares/checkToken.js
@@ -2,22 +2,17 @@ import jwt from "jsonwebtoken";
 
 const checkToken = (req, res, next) => {
   const authHeader = req.header("Authorization");
-  
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  if (!authHeader || !authHeader.startsWith("Bearer "))
     return res.status(401).json({ message: "El Bearer token es necesario" });
-  }
 
   const token = authHeader.replace("Bearer ", "");
-
   try {
     const tokenDecoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = tokenDecoded; 
-    console.log("Usuario autenticado:", tokenDecoded); 
-
-    next(); 
+    console.log(tokenDecoded);
   } catch (err) {
-    return res.status(401).json({ message: "El Bearer token es incorrecto o ha expirado" });
+    return res.status(401).json({ message: "El Bearer token es incorrecto" });
   }
+  next();
 };
 
 export default checkToken;
