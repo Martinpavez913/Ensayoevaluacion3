71f549b0582f2be1eef97d95d0d717338f3113dd
Martinpavez913 <166556020+Martinpavez913@users.noreply.github.com>
2025-11-29 17:09:18 -0300

    cambios en el check token, donde falta el req.user

--- src/middlewares/checkToken.js
+++ src/middlewares/checkToken.js
@@ -2,17 +2,22 @@ import jwt from "jsonwebtoken";
 
 const checkToken = (req, res, next) => {
   const authHeader = req.header("Authorization");
-  if (!authHeader || !authHeader.startsWith("Bearer "))
+  
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "El Bearer token es necesario" });
+  }
 
   const token = authHeader.replace("Bearer ", "");
+
   try {
     const tokenDecoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(tokenDecoded);
+    req.user = tokenDecoded; 
+    console.log("Usuario autenticado:", tokenDecoded); 
+
+    next(); 
   } catch (err) {
-    return res.status(401).json({ message: "El Bearer token es incorrecto" });
+    return res.status(401).json({ message: "El Bearer token es incorrecto o ha expirado" });
   }
-  next();
 };
 
 export default checkToken;
